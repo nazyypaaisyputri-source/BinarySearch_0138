edcf13aafcc8ddd7b7ece19dca0ee7fb6f3f66ff
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-16 11:23:18 +0700

    add repeat search option using do-while loop

--- hmm.cpp
+++ hmm.cpp
@@ -103,5 +103,8 @@ if (low > high)
 {
     cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
 }
+cout << "\nIngin mencari lagi? (y/n): ";
+cin >> ulang;
+
+} while (ulang == 'y' || ulang == 'Y');
     }
-}
